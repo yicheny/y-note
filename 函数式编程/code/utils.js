a891aaa70de376a7ede735b701422ff8632fa54c
yicheny <1463020554@qq.com>
2020-08-19 13:03:22 +0800

    add 类型签名

--- 函数式编程/code/utils.js
+++ 函数式编程/code/utils.js
@@ -1,5 +1,28 @@
+const { curry } = require('lodash');
+
 //用于debug的追踪函数
-var trace = curry(function (tag, x) {
+const trace = curry(function (tag, x) {
     console.log(tag, x);
     return x;
 });
+
+//组合
+const compose = function (f, g) {
+    return function (x) {
+        return f(g(x));
+    };
+};
+
+//
+const match = curry((what, str) => str.match(whate));
+const replace = curry((what, replacement, str) => str.replace(what, replacement));
+const filter = curry((f, ary) => ary.filter(f));
+const map = curry((f, ary) => ary.map(f));
+
+const toUpperCase = x => x.toUpperCase();
+const toLowerCase = x => x.toLowerCase();
+const head = x => x[0];
+const reverse = reduce((acc, x) => [x].concat(acc), []);
+const last = compose(head, reverse);
+
+//
